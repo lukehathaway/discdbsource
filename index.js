a9b4f25b57d0ac8369320486fdf6a7b7d9fcdb96
Luke Hathaway <lukehathaway@gmail.com>
2022-05-17 20:01:06 -0400

    Molds and more endpoints added

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
-const path = require('path');
+const manufacturers = require('./manufacturers.json');
+const molds = require('./molds');
 
 const app = express();
 const port = 8080;
 
 app.use('/api/manufacturers', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.sendFile(path.join(__dirname + '/manufacturers.json'));
+  const allManufacturers = manufacturers.manufacturers.map(manufacturer => {
+    return manufacturer.name;
+  });
+  res.send(allManufacturers);
 });
 
-app.use('/api/molds', (req, res) => {
+app.use('/api/molds/all', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.sendFile(path.join(__dirname + '/molds.json'));
+  const allMoldNames = molds.molds.map(mold => {
+    return mold.name;
+  });
+  res.send(allMoldNames );
 });
+
+app.get('/api/molds/:manufacturer', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const manufacturer = req.params.manufacturer;
+  // TODO: refactor this into one function
+  const filteredMolds = molds.molds.filter(mold => {
+    return mold.manufacturer === manufacturer;
+  });
+  const filteredMoldsNames = filteredMolds.map(mold => {
+    return mold.name;
+  });
+  res.send(filteredMoldsNames)
+});
+
+app.get('/api/molds/:manufacturer/:mold', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const selectedMold = molds.molds.filter(mold => {
+    return mold.manufacturer === req.params.manufacturer && mold.name === req.params.mold;
+  });
+  res.send(selectedMold);
+})
 
 app.listen('8080', () => console.log('Server listening on 8080'));
